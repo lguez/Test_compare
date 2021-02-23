49432dda42a3402b5cae70d4e44895dd92a2a7f3
Lionel GUEZ <guez@lmd.ens.fr>
2021-02-23 18:01:03 +0100

    If not args.silent, display all differences
    Instead of stopping at the first one.

--- nccmp_meta.py
+++ nccmp_meta.py
@@ -8,26 +8,35 @@ parser = argparse.ArgumentParser()
 parser.add_argument("netCDF_file", nargs = 2)
 parser.add_argument("--silent", action="store_true")
 args = parser.parse_args()
+n_diff = 0
 
 def cmp(v1, v2):
     if v1 != v2:
-        if not args.silent:
+        if args.silent:
+            sys.exit(1)
+        else:
             print(v1)
             print(v2)
-            
-        sys.exit(1)
+            return 1
+    else:
+        return 0
 
 f1 = netCDF4.Dataset(args.netCDF_file[0])
 f2 = netCDF4.Dataset(args.netCDF_file[1])
-cmp(f1.__dict__, f2.__dict__)
-cmp(f1.data_model, f2.data_model)
-cmp(f1.disk_format, f2.disk_format)
-cmp(f1.file_format, f2.file_format)
-cmp(f1.dimensions.keys(), f2.dimensions.keys())
-for x in f1.dimensions: cmp(len(f1.dimensions[x]), len(f2.dimensions[x]))
-cmp(f1.variables.keys(), f2.variables.keys())
+n_diff += cmp(f1.__dict__, f2.__dict__)
+n_diff += cmp(f1.data_model, f2.data_model)
+n_diff += cmp(f1.disk_format, f2.disk_format)
+n_diff += cmp(f1.file_format, f2.file_format)
+n_diff += cmp(f1.dimensions.keys(), f2.dimensions.keys())
+
+for x in f1.dimensions:
+    n_diff += cmp(len(f1.dimensions[x]), len(f2.dimensions[x]))
+
+n_diff += cmp(f1.variables.keys(), f2.variables.keys())
 
 for x in f1.variables:
     for attribute in ["__dict__", "dtype", "dimensions", "shape"]:
-        cmp(f1.variables[x].__getattribute__(attribute), 
-            f2.variables[x].__getattribute__(attribute))
+        n_diff += cmp(f1.variables[x].__getattribute__(attribute), 
+                      f2.variables[x].__getattribute__(attribute))
+
+if n_diff != 0: sys.exit(1)
