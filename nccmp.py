fb600aa29a307b319e93cd6043394da0dab95f0d
Lionel GUEZ <guez@lmd.ens.fr>
2021-03-19 19:07:16 +0100

    Uniformize `nccmp.py` and `nccmp_meta.py`
    Rename `nc[12]` to `f[12]` and name to x in `nccmp.py`. Add
    intermediary variables `vars[12]` in `nccmp_meta.py`. Motivation:
    prepare fusion of the files.

--- nccmp.py
+++ nccmp.py
@@ -9,17 +9,18 @@ parser = argparse.ArgumentParser()
 parser.add_argument("netCDF_file", nargs = 2)
 parser.add_argument("--silent", action = "store_true")
 args = parser.parse_args()
-nc1 = netCDF4.Dataset(args.netCDF_file[0])
-nc2 = netCDF4.Dataset(args.netCDF_file[1])
-vars1 = nc1.variables.keys()
-vars2 = nc2.variables.keys()
+
+f1 = netCDF4.Dataset(args.netCDF_file[0])
+f2 = netCDF4.Dataset(args.netCDF_file[1])
+
+vars1 = f1.variables.keys()
+vars2 = f2.variables.keys()
 diff_found = False
 
-for name in vars1 & vars2:
-    diff_found = diff_funct.compare_vars(nc1.variables[name],
-                                         nc2.variables[name],
-                                         args.silent,
-                                         tag = f"Variable {name}") or diff_found
+for x in vars1 & vars2:
+    diff_found = diff_funct.compare_vars(f1.variables[x], f2.variables[x],
+                                         args.silent, tag = f"Variable {x}") \
+                                         or diff_found
     # (Note: call to compare_vars first to avoid short-circuit)
     
     if diff_found and args.silent: break
--- nccmp_meta.py
+++ nccmp_meta.py
@@ -13,6 +13,8 @@ args = parser.parse_args()
 f1 = netCDF4.Dataset(args.netCDF_file[0])
 f2 = netCDF4.Dataset(args.netCDF_file[1])
 
+vars1 = f1.variables.keys()
+vars2 = f2.variables.keys()
 diff_found = diff_funct.diff_dict(f1.__dict__, f2.__dict__, args.silent,
                                   tag = "All attributes of the dataset")
 if args.silent and diff_found: sys.exit(1)
@@ -21,9 +23,7 @@ for tag, v1, v2 in [("Data_model", f1.data_model, f2.data_model),
                     ("Disk_format", f1.disk_format, f2.disk_format),
                     ("File_format", f1.file_format, f2.file_format),
                     ("Dimension names", f1.dimensions.keys(),
-                     f2.dimensions.keys()),
-                    ("Variable names", f1.variables.keys(),
-                     f2.variables.keys())]:
+                     f2.dimensions.keys()), ("Variable names", vars1, vars2)]:
     diff_found = diff_funct.cmp(v1, v2, args.silent, tag) or diff_found
     if diff_found and args.silent: sys.exit(1)
 
@@ -35,19 +35,17 @@ for x in f1.dimensions:
                                     or diff_found
         if diff_found and args.silent: sys.exit(1)
         
-for x in f1.variables:
-    if x in f2.variables:
-        diff_found = diff_funct.diff_dict(f1.variables[x].__dict__,
-                                          f2.variables[x].__dict__, args.silent,
+for x in vars1 & vars2:
+        diff_found = diff_funct.diff_dict(f1[x].__dict__, f2[x].__dict__,
+                                          args.silent,
                                           tag = f"Attributes of variable {x}") \
                                           or diff_found
         if args.silent and diff_found: sys.exit(1)
 
         for attribute in ["dtype", "dimensions", "shape"]:
             diff_found = \
-                diff_funct.cmp(f1.variables[x].__getattribute__(attribute), 
-                               f2.variables[x].__getattribute__(attribute),
-                               args.silent, \
+                diff_funct.cmp(f1[x].__getattribute__(attribute), 
+                               f2[x].__getattribute__(attribute), args.silent, \
                                tag = f"{attribute} of variable {x}") \
                                or diff_found
             if diff_found and args.silent: sys.exit(1)
