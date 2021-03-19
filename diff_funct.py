901737c1db6823e896a8990c3c0cb9ad58acb0aa
Lionel GUEZ <guez@lmd.ens.fr>
2021-03-19 18:47:40 +0100

    Move function `compare_vars` to `diff_funct.py`

--- diff_funct.py
+++ diff_funct.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def cmp(v1, v2, silent = False, tag = None):
     diff_found = v1 != v2
     
@@ -35,5 +37,36 @@ def diff_dict(d1, d2, silent = False, tag = None):
                 print()
                 print(d2[k])
                 print("-----------\n")
+
+    return diff_found
+
+def compare_vars(v1, v2, silent = False, tag = None):
+    """Return True if a difference if found."""
+    
+    if v1.shape != v2.shape:
+        if not silent:
+            if tag: print(tag, ":")
+            print("Different shapes in the two files")
+            print("-------------\n")
+
+        diff_found = True
+    else:
+        if v1.size == 0:
+            if not silent:
+                if tag: print(tag, ":")
+                print('0 size')
+                print("-------------\n")
+                
+            diff_found = False
+        else:
+            if np.any(v1[:] != v2[:]):
+                if not silent:
+                    if tag: print(tag, ":")
+                    print("Different content")
+                    print("-------------\n")
+                    
+                diff_found = True
+            else:
+                diff_found = False
 
     return diff_found
--- nccmp.py
+++ nccmp.py
@@ -3,40 +3,9 @@
 """From nccmp_pism.py."""
 
 import netCDF4
-import numpy as np
 import sys
 import argparse
-
-def compare_vars(v1, v2, silent = False, tag = None):
-    """Return True if a difference if found."""
-    
-    if v1.shape != v2.shape:
-        if not silent:
-            if tag: print(tag, ":")
-            print("Different shapes in the two files")
-            print("-------------\n")
-
-        diff_found = True
-    else:
-        if v1.size == 0:
-            if not silent:
-                if tag: print(tag, ":")
-                print('0 size')
-                print("-------------\n")
-                
-            diff_found = False
-        else:
-            if np.any(v1[:] != v2[:]):
-                if not silent:
-                    if tag: print(tag, ":")
-                    print("Different content")
-                    print("-------------\n")
-                    
-                diff_found = True
-            else:
-                diff_found = False
-
-    return diff_found
+import diff_funct
 
 parser = argparse.ArgumentParser()
 parser.add_argument("netCDF_file", nargs = 2)
@@ -49,9 +18,10 @@ vars2 = nc2.variables.keys()
 diff_found = False
 
 for name in vars1 & vars2:
-    diff_found = compare_vars(nc1.variables[name], nc2.variables[name],
-                              args.silent, tag = f"Variable {name}") \
-                              or diff_found
+    diff_found = diff_funct.compare_vars(nc1.variables[name],
+                                         nc2.variables[name],
+                                         args.silent,
+                                         tag = f"Variable {name}") or diff_found
     # (Note: call to compare_vars first to avoid short-circuit)
     
     if diff_found and args.silent: break
