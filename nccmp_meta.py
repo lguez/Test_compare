4ffe4ce2499b86ddd082289427f21e7a0ae4a77a
Lionel GUEZ <guez@lmd.ens.fr>
2021-03-19 16:37:15 +0100

    Do not call `sys.exit` from the function
    Better structure.

--- nccmp_meta.py
+++ nccmp_meta.py
@@ -6,18 +6,16 @@ import argparse
 import jumble
 
 def cmp(tag, v1, v2):
-    if v1 != v2:
-        if args.silent:
-            sys.exit(1)
-        else:
-            print(tag, ":\n")
-            print(v1)
-            print()
-            print(v2)
-            print("-------------\n")
-            return True
-    else:
-        return False
+    diff_found = v1 != v2
+    
+    if diff_found and not args.silent:
+        print(tag, ":\n")
+        print(v1)
+        print()
+        print(v2)
+        print("-------------\n")
+
+    return diff_found
 
 parser = argparse.ArgumentParser()
 parser.add_argument("netCDF_file", nargs = 2)
@@ -31,18 +29,24 @@ diff_found = jumble.diff_dict(f1.__dict__, f2.__dict__, args.silent,
                               tag = "All attributes of the dataset")
 if args.silent and diff_found: sys.exit(1)
 diff_found = cmp("Data_model", f1.data_model, f2.data_model) or diff_found
+if diff_found and args.silent: sys.exit(1)
 diff_found = cmp("Disk_format", f1.disk_format, f2.disk_format) or diff_found
+if diff_found and args.silent: sys.exit(1)
 diff_found = cmp("File_format", f1.file_format, f2.file_format) or diff_found
+if diff_found and args.silent: sys.exit(1)
 diff_found = cmp("Dimension names", f1.dimensions.keys(),
                  f2.dimensions.keys()) or diff_found
+if diff_found and args.silent: sys.exit(1)
 
 for x in f1.dimensions:
     if x in f2.dimensions:
         diff_found = cmp(f"Size of dimension {x}", len(f1.dimensions[x]),
                          len(f2.dimensions[x])) or diff_found
+        if diff_found and args.silent: sys.exit(1)
         
 diff_found = cmp("Variable names", f1.variables.keys(), f2.variables.keys()) \
     or diff_found
+if diff_found and args.silent: sys.exit(1)
 
 for x in f1.variables:
     if x in f2.variables:
@@ -58,5 +62,4 @@ for x in f1.variables:
                              f1.variables[x].__getattribute__(attribute), 
                              f2.variables[x].__getattribute__(attribute)) \
                              or diff_found
-
-if diff_found: sys.exit(1)
+            if diff_found and args.silent: sys.exit(1)
