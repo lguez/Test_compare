62a5d67f286c2b68c78d66256c92b951375878e6
Lionel GUEZ <guez@lmd.ens.fr>
2021-03-19 18:27:19 +0100

    Change interface of function `compare_vars`
    Motivation: uniformize with `diff_funct`.

--- nccmp.py
+++ nccmp.py
@@ -7,25 +7,21 @@ import numpy as np
 import sys
 import argparse
 
-def compare_vars(nc1, nc2, name):
-    """Return True if a difference if found. name must be a variable
-    common to nc1 and nc2."""
+def compare_vars(v1, v2, tag):
+    """Return True if a difference if found."""
     
-    var1 = nc1.variables[name]
-    var2 = nc2.variables[name]
-
-    if var1.shape != var2.shape:
+    if v1.shape != v2.shape:
         if not args.silent:
-            print(f"Variable {name}, different shapes in the two files")
+            print(f"{tag}, different shapes in the two files")
 
         diff_found = True
     else:
-        if var1.size == 0:
-            if not args.silent: print(f'Variable {name}: 0 size.')
+        if v1.size == 0:
+            if not args.silent: print(f'{tag}: 0 size.')
             diff_found = False
         else:
-            if np.any(var1[:] != var2[:]):
-                if not args.silent: print(f"Variable {name}, different content")
+            if np.any(v1[:] != v2[:]):
+                if not args.silent: print(f"{tag}, different content")
                 diff_found = True
             else:
                 diff_found = False
@@ -43,7 +39,8 @@ vars2 = nc2.variables.keys()
 diff_found = False
 
 for name in vars1 & vars2:
-    diff_found = compare_vars(nc1, nc2, name) or diff_found
+    diff_found = compare_vars(nc1.variables[name], nc2.variables[name],
+                              tag = f"Variable {name}") or diff_found
     # (Note: call to compare_vars first to avoid short-circuit)
     
     if diff_found and args.silent: break
