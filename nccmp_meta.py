55c92bf02afbe247fa10629fa87f2f15fc0e7c1f
Lionel GUEZ <guez@lmd.ens.fr>
2021-03-19 16:20:05 +0100

    Use `diff_found` instead of `n_diff`
    Use logical `diff_found` instead of integer `n_diff`. We do not use
    the number of differences.

--- nccmp_meta.py
+++ nccmp_meta.py
@@ -15,45 +15,48 @@ def cmp(tag, v1, v2):
             print()
             print(v2)
             print("-------------\n")
-            return 1
+            return True
     else:
-        return 0
+        return False
 
 parser = argparse.ArgumentParser()
 parser.add_argument("netCDF_file", nargs = 2)
 parser.add_argument("--silent", action="store_true")
 args = parser.parse_args()
-n_diff = 0
 
 f1 = netCDF4.Dataset(args.netCDF_file[0])
 f2 = netCDF4.Dataset(args.netCDF_file[1])
 
-n_diff += jumble.diff_dict(f1.__dict__, f2.__dict__, args.silent,
-                           tag = "All attributes of the dataset")
-if args.silent and n_diff != 0: sys.exit(1)
-n_diff += cmp("Data_model", f1.data_model, f2.data_model)
-n_diff += cmp("Disk_format", f1.disk_format, f2.disk_format)
-n_diff += cmp("File_format", f1.file_format, f2.file_format)
-n_diff += cmp("Dimension names", f1.dimensions.keys(), f2.dimensions.keys())
+diff_found = jumble.diff_dict(f1.__dict__, f2.__dict__, args.silent,
+                              tag = "All attributes of the dataset")
+if args.silent and diff_found: sys.exit(1)
+diff_found = cmp("Data_model", f1.data_model, f2.data_model) or diff_found
+diff_found = cmp("Disk_format", f1.disk_format, f2.disk_format) or diff_found
+diff_found = cmp("File_format", f1.file_format, f2.file_format) or diff_found
+diff_found = cmp("Dimension names", f1.dimensions.keys(),
+                 f2.dimensions.keys()) or diff_found
 
 for x in f1.dimensions:
     if x in f2.dimensions:
-        n_diff += cmp(f"Size of dimension {x}", len(f1.dimensions[x]),
-                      len(f2.dimensions[x]))
+        diff_found = cmp(f"Size of dimension {x}", len(f1.dimensions[x]),
+                         len(f2.dimensions[x])) or diff_found
         
-n_diff += cmp("Variable names", f1.variables.keys(), f2.variables.keys())
+diff_found = cmp("Variable names", f1.variables.keys(), f2.variables.keys()) \
+    or diff_found
 
 for x in f1.variables:
     if x in f2.variables:
-        n_diff += jumble.diff_dict(f1.variables[x].__dict__,
-                                   f2.variables[x].__dict__, args.silent,
-                                   tag = f"Attributes of variable {x}")
+        diff_found = jumble.diff_dict(f1.variables[x].__dict__,
+                                      f2.variables[x].__dict__, args.silent,
+                                      tag = f"Attributes of variable {x}") \
+                                      or diff_found
     
-        if args.silent and n_diff != 0: sys.exit(1)
+        if args.silent and diff_found: sys.exit(1)
 
         for attribute in ["dtype", "dimensions", "shape"]:
-            n_diff += cmp(f"{attribute} of variable {x}",
-                          f1.variables[x].__getattribute__(attribute), 
-                          f2.variables[x].__getattribute__(attribute))
+            diff_found = cmp(f"{attribute} of variable {x}",
+                             f1.variables[x].__getattribute__(attribute), 
+                             f2.variables[x].__getattribute__(attribute)) \
+                             or diff_found
 
-if n_diff != 0: sys.exit(1)
+if diff_found: sys.exit(1)
