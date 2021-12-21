24d40089cc80b00b08215291152af196836f7cf8
Lionel GUEZ <guez@lmd.ens.fr>
2021-12-21 14:21:22 +0100

    Create function `nccmp`
    So we can call it from another script.

--- nccmp.py
+++ nccmp.py
@@ -7,6 +7,63 @@ import sys
 import argparse
 import compare_util
 
+def nccmp():
+    f1 = netCDF4.Dataset(args.netCDF_file[0])
+    f2 = netCDF4.Dataset(args.netCDF_file[1])
+
+    vars1 = f1.variables.keys()
+    vars2 = f2.variables.keys()
+
+    if args.data:
+        diff_found = False
+    else:
+        diff_found = compare_util.diff_dict(f1.__dict__, f2.__dict__, args.silent,
+                                            tag = "All attributes of the dataset")
+        if args.silent and diff_found: sys.exit(1)
+
+        for tag, v1, v2 in [("Data_model", f1.data_model, f2.data_model),
+                            ("Disk_format", f1.disk_format, f2.disk_format),
+                            ("File_format", f1.file_format, f2.file_format),
+                            ("Dimension names", f1.dimensions.keys(),
+                             f2.dimensions.keys()), ("Variable names", vars1,
+                                                     vars2)]:
+            diff_found = compare_util.cmp(v1, v2, args.silent, tag) or diff_found
+            if diff_found and args.silent: sys.exit(1)
+
+        for x in f1.dimensions:
+            if x in f2.dimensions:
+                diff_found = compare_util.cmp(len(f1.dimensions[x]),
+                                              len(f2.dimensions[x]), args.silent,
+                                              tag = f"Size of dimension {x}") \
+                    or diff_found
+                if diff_found and args.silent: sys.exit(1)
+
+        for x in vars1 & vars2:
+                diff_found \
+                    = compare_util.diff_dict(f1[x].__dict__, f2[x].__dict__,
+                                             args.silent,
+                                             tag = f"Attributes of variable {x}") \
+                    or diff_found
+                if args.silent and diff_found: sys.exit(1)
+
+                for attribute in ["dtype", "dimensions", "shape"]:
+                    diff_found = \
+                        compare_util.cmp(f1[x].__getattribute__(attribute), 
+                                         f2[x].__getattribute__(attribute),
+                                         args.silent, \
+                                         tag = f"{attribute} of variable {x}") \
+                            or diff_found
+                    if diff_found and args.silent: sys.exit(1)
+
+    for x in vars1 & vars2:
+        diff_found = compare_util.compare_vars(f1[x], f2[x], args.silent,
+                                               tag = f"Variable {x}") or diff_found
+        # (Note: call to compare_vars first to avoid short-circuit)
+
+        if diff_found and args.silent: break
+
+    if diff_found: sys.exit(1)
+
 parser = argparse.ArgumentParser()
 parser.add_argument("netCDF_file", nargs = 2)
 parser.add_argument("-s", "--silent", action = "store_true")
@@ -14,58 +71,4 @@ parser.add_argument("-d", "--data", action = "store_true",
                     help = "compare only data")
 args = parser.parse_args()
 
-f1 = netCDF4.Dataset(args.netCDF_file[0])
-f2 = netCDF4.Dataset(args.netCDF_file[1])
-
-vars1 = f1.variables.keys()
-vars2 = f2.variables.keys()
-
-if args.data:
-    diff_found = False
-else:
-    diff_found = compare_util.diff_dict(f1.__dict__, f2.__dict__, args.silent,
-                                        tag = "All attributes of the dataset")
-    if args.silent and diff_found: sys.exit(1)
-
-    for tag, v1, v2 in [("Data_model", f1.data_model, f2.data_model),
-                        ("Disk_format", f1.disk_format, f2.disk_format),
-                        ("File_format", f1.file_format, f2.file_format),
-                        ("Dimension names", f1.dimensions.keys(),
-                         f2.dimensions.keys()), ("Variable names", vars1,
-                                                 vars2)]:
-        diff_found = compare_util.cmp(v1, v2, args.silent, tag) or diff_found
-        if diff_found and args.silent: sys.exit(1)
-
-    for x in f1.dimensions:
-        if x in f2.dimensions:
-            diff_found = compare_util.cmp(len(f1.dimensions[x]),
-                                          len(f2.dimensions[x]), args.silent,
-                                          tag = f"Size of dimension {x}") \
-                or diff_found
-            if diff_found and args.silent: sys.exit(1)
-
-    for x in vars1 & vars2:
-            diff_found \
-                = compare_util.diff_dict(f1[x].__dict__, f2[x].__dict__,
-                                         args.silent,
-                                         tag = f"Attributes of variable {x}") \
-                or diff_found
-            if args.silent and diff_found: sys.exit(1)
-
-            for attribute in ["dtype", "dimensions", "shape"]:
-                diff_found = \
-                    compare_util.cmp(f1[x].__getattribute__(attribute), 
-                                     f2[x].__getattribute__(attribute),
-                                     args.silent, \
-                                     tag = f"{attribute} of variable {x}") \
-                        or diff_found
-                if diff_found and args.silent: sys.exit(1)
-
-for x in vars1 & vars2:
-    diff_found = compare_util.compare_vars(f1[x], f2[x], args.silent,
-                                           tag = f"Variable {x}") or diff_found
-    # (Note: call to compare_vars first to avoid short-circuit)
-    
-    if diff_found and args.silent: break
-
-if diff_found: sys.exit(1)
+nccmp()
