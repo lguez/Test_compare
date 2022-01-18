bb80dcf2052d096b16bf1fd7daa3815c27653789
Lionel GUEZ <guez@lmd.ens.fr>
2022-01-18 16:34:15 +0100

    Change the structure of nccmp so it does not exit
    Change the structure of function nccmp so it does not exit but returns
    0 or 1.

--- nccmp.py
+++ nccmp.py
@@ -3,7 +3,6 @@
 """Requires Python >= 3.6."""
 
 import netCDF4
-import sys
 import compare_util
 
 def nccmp(old, new, silent = False, data_only = False):
@@ -19,33 +18,35 @@ def nccmp(old, new, silent = False, data_only = False):
         diff_found = compare_util.diff_dict(f1.__dict__, f2.__dict__, silent,
                                             tag = "All attributes of the "
                                             "dataset")
-        if silent and diff_found: sys.exit(1)
+        if not silent or not diff_found:
+            for tag, v1, v2 in [("Data_model", f1.data_model, f2.data_model),
+                                ("Disk_format", f1.disk_format, f2.disk_format),
+                                ("File_format", f1.file_format, f2.file_format),
+                                ("Dimension names", f1.dimensions.keys(),
+                                 f2.dimensions.keys()),
+                                ("Variable names", vars1, vars2)]:
+                diff_found = compare_util.cmp(v1, v2, silent, tag) or diff_found
+                if diff_found and silent: break
 
-        for tag, v1, v2 in [("Data_model", f1.data_model, f2.data_model),
-                            ("Disk_format", f1.disk_format, f2.disk_format),
-                            ("File_format", f1.file_format, f2.file_format),
-                            ("Dimension names", f1.dimensions.keys(),
-                             f2.dimensions.keys()), ("Variable names", vars1,
-                                                     vars2)]:
-            diff_found = compare_util.cmp(v1, v2, silent, tag) or diff_found
-            if diff_found and silent: sys.exit(1)
+        if not silent or not diff_found:
+            for x in f1.dimensions:
+                if x in f2.dimensions:
+                    diff_found = compare_util.cmp(len(f1.dimensions[x]),
+                                                  len(f2.dimensions[x]), silent,
+                                                  tag = "Size of dimension "
+                                                  f"{x}") or diff_found
+                    if diff_found and silent: break
 
-        for x in f1.dimensions:
-            if x in f2.dimensions:
-                diff_found = compare_util.cmp(len(f1.dimensions[x]),
-                                              len(f2.dimensions[x]), silent,
-                                              tag = f"Size of dimension {x}") \
-                    or diff_found
-                if diff_found and silent: sys.exit(1)
+        inters_vars = vars1 & vars2
 
-        for x in vars1 & vars2:
-                diff_found \
-                    = compare_util.diff_dict(f1[x].__dict__, f2[x].__dict__,
-                                             silent,
-                                             tag = "Attributes of variable "
-                                             f"{x}") or diff_found
-                if silent and diff_found: sys.exit(1)
+        while len(inters_vars) != 0 and (not silent or not diff_found):
+            x = inters_vars.pop()
+            diff_found \
+                = compare_util.diff_dict(f1[x].__dict__, f2[x].__dict__,
+                                         silent, tag = "Attributes of "
+                                         f"variable {x}") or diff_found
 
+            if not silent or not diff_found:
                 for attribute in ["dtype", "dimensions", "shape"]:
                     diff_found = \
                         compare_util.cmp(f1[x].__getattribute__(attribute), 
@@ -53,20 +54,25 @@ def nccmp(old, new, silent = False, data_only = False):
                                          silent, \
                                          tag = f"{attribute} of variable {x}") \
                             or diff_found
-                    if diff_found and silent: sys.exit(1)
+                    if diff_found and silent: break
 
-    for x in vars1 & vars2:
-        diff_found = compare_util.compare_vars(f1[x], f2[x], silent,
-                                               tag = f"Variable {x}") \
-                                               or diff_found
-        # (Note: call to compare_vars first to avoid short-circuit)
+    if not silent or not diff_found:
+        for x in vars1 & vars2:
+            diff_found = compare_util.compare_vars(f1[x], f2[x], silent,
+                                                   tag = f"Variable {x}") \
+                                                   or diff_found
+            # (Note: call to compare_vars first to avoid short-circuit)
 
-        if diff_found and silent: break
+            if diff_found and silent: break
 
-    if diff_found: sys.exit(1)
+    if diff_found:
+        return 1
+    else:
+        return 0
 
 if __name__ == "__main__":
     import argparse
+    import sys
     
     parser = argparse.ArgumentParser()
     parser.add_argument("netCDF_file", nargs = 2)
@@ -75,4 +81,5 @@ if __name__ == "__main__":
                         help = "compare only data")
     args = parser.parse_args()
 
-    nccmp(*args.netCDF_file, args.silent, args.data_only)
+    nccmp_return = nccmp(*args.netCDF_file, args.silent, args.data_only)
+    sys.exit(nccmp_return)
