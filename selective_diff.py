dad6036e5bfc985e3fbf3baf404d4e8e2a79abcf
Lionel GUEZ <guez@lmd.ens.fr>
2022-04-07 16:51:21 +0200

    Rewrite `selective_diff.py` using `filecmp.dircmp`
    Also creating a `detailed_diff` class. One motivation is to be able to
    plug various functions for a given file-type. So an instance of
    `detailed_diff` is configured with a choice of functions for the
    interesting file-types.

--- selective_diff.py
+++ selective_diff.py
@@ -1,224 +1,169 @@
 #!/usr/bin/env python3
 
-"""Author: Lionel GUEZ
-
-Selective analysis of differences between directories based on file
-type and amount of difference. This script first compares briefly
-two directories. The two directories should be given as arguments of
-the script. If the directories contains text or DBF files with the
-same name and modestly different content then the script prints the
-detailed difference between them. If the directories contain NetCDF
-files with the same name and different content then the script
-compares further the header and data parts. The script prints the
-detailed differences between the header part, and between the dumps
-of the data parts if the number of different lines is small
-enough. The script optionally:
-
-- creates a NetCDF file with the NCO operator "ncbo" containing the
-difference and the relative difference between the original NetCDF
-files;
-
-- computes statistics of the differences.
-
-The user of this script needs to have the permission to write in the
-current directory.
-
-Non-standard utilities invoked in this script: ncdump, NCO,
-nccmp.py, max_diff_nc.sh, dbfdump, ndiff by Nelson Beebe,
-numdiff. ndiff and numdiff are not completely redundant.
-
-An exit status of 0 means no differences were found, 1 means some
-differences were found, and 2 means trouble.
-
-"""
-
-import netCDF4
-from os import path
 import argparse
 import pathlib
 import sys
-import os
-import re
-import subprocess
 import filecmp
+import difflib
+from os import path
 import magic
 import diff_dbf
+import subprocess
+import os
+import tempfile
+import nccmp
 import fnmatch
 
 def cat_not_too_many(file, size_lim):
+    """file should be an existing file object."""
+
     count = 0
-    
-    with open(file) as f:
-        while True:
-            line = f.readline()
-            count += 1
-            if line == "" or count > size_lim: break
+    file.seek(0)
+    while True:
+        line = file.readline()
+        count += 1
+        if line == "" or count > size_lim: break
 
-        if count < size_lim:
-            f.seek(0)
-            for line in f: print(line, end = "")
-            print()
+    if count <= size_lim:
+        file.seek(0)
+        for line in file: print(line, end = "")
+        print()
+    else:
+        print("Too many lines in diff output")
+
+def diff_txt(path_1, path_2, size_lim):
+    """Treat path_1 and path_2 as text files."""
+
+    with open(path_1) as f: fromlines = f.readlines()
+    with open(path_2) as f: tolines = f.readlines()
+    diff = difflib.unified_diff(fromlines, tolines, fromfile = path_1,
+                                tofile = path_2)
+
+    with tempfile.TemporaryFile("w+") as diff_out:
+        diff_out.writelines(diff)
+        cat_not_too_many(diff_out, size_lim)
+
+    print()
+    return 1
+
+def diff_csv(path_1, path_2, size_lim):
+
+    with tempfile.TemporaryFile("w+") as ndiff_out:
+        cp = subprocess.run(["ndiff", "-relerr", "1e-7", path_1, path_2],
+                            stdout = ndiff_out, text = True)
+        cat_not_too_many(ndiff_out, size_lim)
+
+    print()
+    return cp.returncode
+
+def my_report(dcmp, detailed_diff_instance):
+    print()
+    dcmp.report()
+    n_diff = len(dcmp.left_only) + len(dcmp.right_only) \
+        + len(dcmp.common_funny) + len(dcmp.funny_files)
+
+    if detailed_diff_instance is None:
+        n_diff += len(dcmp.diff_files)
+    else:
+        for name in dcmp.diff_files:
+            path_1 = path.join(dcmp.left, name)
+            path_2 = path.join(dcmp.right, name)
+            n_diff += detailed_diff_instance.diff(path_1, path_2)
+
+    for sub_dcmp in dcmp.subdirs.values():
+        n_diff += my_report(sub_dcmp, detailed_diff_instance)
+
+    return n_diff
+
+class detailed_diff:
+    def __init__(self, size_lim, diff_dbf = None):
+        if diff_dbf is None:
+            self.diff_dbf = self.diff_dbf_dbfdump
         else:
-            print("Too many lines in diff output")
+            self.diff_dbf = diff_dbf
 
+        self.size_lim = size_lim
+
+    def diff(self, path_1, path_2):
+        print('\n*******************************************\n')
+        print(path_1, path_2)
+        suffix = pathlib.PurePath(path_1).suffix
+        text_file = suffix == ".txt" or suffix == ".json" \
+            or (suffix != ".nc" and suffix != ".csv"
+                and "text" in magic.from_file(path_1))
+
+        if text_file:
+            n_diff = diff_txt(path_1, path_2, self.size_lim)
+        elif suffix == ".dbf":
+            n_diff = self.diff_dbf(path_1, path_2)
+        elif suffix == ".csv":
+            n_diff = diff_csv(path_1, path_2, self.size_lim)
+        elif suffix == ".nc":
+            n_diff = nccmp.nccmp(path_1, path_2)
+        else:
+            print("Detailed diff not implemented")
+            n_diff = 1
+
+        return n_diff
+
+    def diff_dbf_dbfdump(self, path_1, path_2):
+        f1_dbfdump = tempfile.NamedTemporaryFile("w+")
+        f2_dbfdump = tempfile.NamedTemporaryFile("w+")
+        subprocess.run(["dbfdump", path_1], stdout = f1_dbfdump)
+        subprocess.run(["dbfdump", path_2], stdout = f2_dbfdump)
+
+        if filecmp.cmp(f1_dbfdump.name, f2_dbfdump.name,
+                       shallow = False):
+            print(f"dbfdumps of {path_1} and {path_2} are identical")
+            n_diff = 0
+        else:
+            n_diff = diff_csv(f1_dbfdump.name, f2_dbfdump.name,
+                              self.size_lim)
+        f1_dbfdump.close()
+        f2_dbfdump.close()
+        return n_diff
 
 parser = argparse.ArgumentParser()
 parser.add_argument("directory", nargs = 2)
-parser.add_argument("-d", "--subtract",
-                    help = "create a NetCDF file containing differences "
-                    "for NetCDF files with different data parts",
-                    action = "store_true")
+parser.add_argument("--pyshp", action = "store_true")
 parser.add_argument("-l", "--limit", help = "maximum number of lines for "
                     "printing detailed differences (default 50)", type = int,
                     default = 50)
-parser.add_argument("-s", "--statistics",
-                    help = "compute statistics for NetCDF files with "
-                    "different data parts", action = "store_true")
 parser.add_argument("-b", "--brief",
                     help = "only compare directories briefly (default: "
                     "analyse each file after brief comparison of directories)",
                     action = "store_true")
-parser.add_argument("-r", "--report_identical",
-                    help = "report indentical directories",
-                    action = "store_true")
-parser.add_argument("-x", metavar = 'PAT', action = "append", default = [],
+parser.add_argument("-x", "--exclude", metavar = 'PAT', action = "append",
+                    default = [],
                     help = "exclude files that match shell pattern PAT")
 args = parser.parse_args()
-dir1 = pathlib.Path(args.directory[0])
-dir2 = pathlib.Path(args.directory[1])
 
-if not dir1.is_dir() or not dir2.is_dir():
+if not path.isdir(args.directory[0]) or not path.isdir(args.directory[1]):
     print()
-    print("Directories: ", *args.directory)
-    sys.exit("Bad directories")
+    print("Bad directories: ", *args.directory)
+    sys.exit(2)
 
-# Compare directories briefly:
+# Construct a list of files to ignore:
 
-exclude_options = []
+ignore = set()
 
-for p in args.x: exclude_options.extend(["-x", p])
+for my_dir in args.directory:
+    for dirpath, dirnames, filenames in os.walk(my_dir):
+        for pattern in args.exclude:
+            list_match = fnmatch.filter(filenames, pattern)
+            ignore.update(list_match)
 
-p_diff = subprocess.Popen(["diff", "--recursive", "--brief"] + exclude_options
-                           + [dir1, dir2], text = True,
-                          stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
-completed_process = subprocess.run(["grep", "^Only in "], text = True,
-                                   stdin = p_diff.stdout, capture_output = True)
+dcmp = filecmp.dircmp(*args.directory, list(ignore))
 
-n_diff=0
-# (number of differing files)
+if args.brief:
+    detailed_diff_instance = None
+else:
+    if args.pyshp:
+        detailed_diff_instance = detailed_diff(args.limit,
+                                               diff_dbf = diff_dbf.diff_dbf)
+    else:
+        detailed_diff_instance = detailed_diff(args.limit)
 
-n_id=0
-# (number of identical files)
-
-initial_dir = os.getcwd()
-os.chdir(dir1)
-file_list = []
-
-for dirpath, dirnames, filenames in os.walk(os.curdir):
-    for filename in filenames:
-        my_path = path.join(dirpath, filename)
-
-        if not path.islink(my_path):
-            for p in args.x:
-                if fnmatch.fnmatch(filename, p): break
-            else:
-                file_list.append(my_path)
-
-os.chdir(initial_dir)
-diff_file = []
-id_file = []
-
-for filename in file_list:
-    path_2 = path.join(dir2, filename)
-
-    if os.access(path_2, os.F_OK):
-        # We have a file with the same name in the two directories
-        path_1 = path.join(dir1, filename)
-        equal = filecmp.cmp(path_1, path_2, shallow = False)
-        
-        if not equal and not args.brief:
-            # Sometimes the files have the same content but are not
-            # identical, so double-check:
-            suffix = pathlib.PurePath(filename).suffix
-
-            if suffix == ".nc":
-                equal = nccmp.nccmp(path_1, path_2, silent = True) == 0
-            elif suffix == ".dbf":
-                equal = diff_dbf.diff_dbf(path_1, path_2, quiet = True) == 0
-
-        if not equal:
-            # Different content
-            n_diff += 1
-            diff_file.append(filename)
-        else:
-            # Same content
-            n_id += 1
-            id_file.append(filename)
-
-if args.report_identical or len(completed_process.stdout) != 0 or n_diff != 0:
-    print("\nDirectories:", *args.directory)
-    print(completed_process.stdout)
-    print("Differing files:")
-    print()
-    print(*diff_file, sep = '\n')
-    print()
-    print("Number of files found:")
-    print(len(diff_file))
-    print()
-    print("Identical files:")
-    print()
-    print(*id_file, sep = "\n")
-    print()
-    print("Number of files found:")
-    print(len(id_file))
-    print()
-
-    if not args.brief:
-        # Analyse each file
-        for name in diff_file:
-            path_1 = path.join(dir1, name)
-            path_2 = path.join(dir2, name)
-            if not os.access(path_1, os.F_OK):
-                print("Broken link:", path_1)
-            elif not os.access(path_2, os.F_OK):
-                print("Broken link:", path_2)
-            else:
-                suffix = pathlib.PurePath(name).suffix
-                text_file = suffix == ".txt" or suffix == ".json" \
-                    or (suffix != ".nc" and suffix != ".csv"
-                        and "text" in magic.from_file(path_1))
-
-                if text_file or re.fullmatch(".nc|.dbf|.csv", suffix):
-                    # We have a text, NetCDF, DBF or CSV file
-                    name0 = pathlib.Path(name).stem
-                    print()
-                    print('*******************************************')
-                    print()
-                    print(name)
-
-                    if text_file:
-                        with open("diff_out", "w") as f:
-                            subprocess.run(["diff", "--ignore-all-space",
-                                            path_1, path_2], stdout = f,
-                                           text = True)
-                        
-                        if path.getsize("diff_out") == 0:
-                            print("Only white space difference")
-                        else:
-                            cat_not_too_many("diff_out", args.limit)
-                        
-                        print()
-                        os.remove("diff_out")
-                    elif suffix == ".dbf":
-                        diff_dbf.diff_dbf(path_1, path_2)
-                    elif suffix == ".csv":
-                        with open("ndiff_out", "w") as f:
-                            subprocess.run(["ndiff", "-relerr", "1e-7", path_1,
-                                            path_2], stdout = f, text = True)
-                        
-                        cat_not_too_many("ndiff_out", args.limit)
-                        print()
-                    else:
-                        nccmp(path_1, path_2)
-
-if n_diff != 0 or len(completed_process.stdout) != 0: sys.exit(1)
+n_diff = my_report(dcmp, detailed_diff_instance)
+print("\nNumber of differences:", n_diff)
+if n_diff != 0: sys.exit(1)
