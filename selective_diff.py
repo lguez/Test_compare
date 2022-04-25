9cc903f6db273a7e136b9809cdef9ed87fcc8a4a
Lionel GUEZ <guez@lmd.ens.fr>
2022-04-25 14:35:53 +0200

    Polish

--- selective_diff.py
+++ selective_diff.py
@@ -72,7 +72,7 @@ def my_report(dcmp, detailed_diff_instance):
         n_diff += my_report(sub_dcmp, detailed_diff_instance)
 
     if n_diff != 0:
-        print()
+        print('\n*******************************************\n')
         dcmp.report()
 
     return n_diff
