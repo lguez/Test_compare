1547d3ee4e8ec327921199abfa22bae9bdd7fc6c
Lionel GUEZ <guez@lmd.ens.fr>
2022-05-17 00:21:45 +0200

    Allow comparison of files containing groups
    With recursive calling of nccmp.

--- nccmp.py
+++ nccmp.py
@@ -19,13 +19,17 @@ def nccmp(f1, f2, silent = False, data_only = False):
         diff_found = compare_util.diff_dict(f1.__dict__, f2.__dict__, silent,
                                             tag = "All attributes of the "
                                             "dataset")
+        groups1 = f1.groups.keys()
+        groups2 = f2.groups.keys()
+
         if not silent or not diff_found:
             for tag, v1, v2 in [("Data_model", f1.data_model, f2.data_model),
                                 ("Disk_format", f1.disk_format, f2.disk_format),
                                 ("File_format", f1.file_format, f2.file_format),
                                 ("Dimension names", f1.dimensions.keys(),
                                  f2.dimensions.keys()),
-                                ("Variable names", vars1, vars2)]:
+                                ("Variable names", vars1, vars2),
+                                ("Group names", groups1, groups2)]:
                 diff_found = compare_util.cmp(v1, v2, silent, tag) or diff_found
                 if diff_found and silent: break
 
@@ -56,6 +60,13 @@ def nccmp(f1, f2, silent = False, data_only = False):
                                          tag = f"{attribute} of variable {x}") \
                             or diff_found
                     if diff_found and silent: break
+
+        inters_groups = groups1 & groups2
+
+        while len(inters_groups) != 0 and (not silent or not diff_found):
+            x = inters_groups.pop()
+            diff_found = nccmp(f1[x], f2[x], silent, data_only) == 1 \
+                or diff_found
 
     if not silent or not diff_found:
         for x in vars1 & vars2:
