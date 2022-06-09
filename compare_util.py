9aedbc7f0a1705f89d3a0ec30783af2f2de6cb61
Lionel GUEZ <guez@lmd.ens.fr>
2022-06-09 14:40:08 +0200

    Allow output to any file object
    Generalize the functions with an additional file argument. This is
    useful when we call the functions from `selective_diff.py` and we want
    to add a header only if the functions find some differences.

--- compare_util.py
+++ compare_util.py
@@ -1,56 +1,54 @@
 import numpy as np
 from numpy import ma
+import sys
 
-def cmp(v1, v2, silent = False, tag = None):
+def cmp(v1, v2, silent = False, tag = None, detail_file = sys.stdout):
     diff_found = v1 != v2
     
     if diff_found and not silent:
-        if tag: print(tag, ":\n")
-        print(v1)
-        print()
-        print(v2)
-        print("-------------\n")
+        if tag: detail_file.write(f"{tag}:\n\n")
+        detail_file.write(f"{v1}\n\n")
+        detail_file.write(f"{v2}\n")
+        detail_file.write("-------------\n\n")
 
     return diff_found
 
-def diff_dict(d1, d2, silent = False, tag = None):
+def diff_dict(d1, d2, silent = False, tag = None, detail_file = sys.stdout):
     diff_found = dict(d1) != dict(d2)
     
     if diff_found and not silent:
-        if tag: print(tag, ":\n")
+        if tag: detail_file.write(f"{tag}:\n\n")
         keys_1 = d1.keys()
         keys_2 = d2.keys()
         exclusive_keys = keys_1 ^ keys_2
 
         for k in exclusive_keys:
             if k in d1:
-                print(k, "in first dictionary only")
+                detail_file.write(f"{k} in first dictionary only\n")
             else:
-                 print(k, "in second dictionary only")
+                 detail_file.write(f"{k} in second dictionary only\n")
 
-            print("-----------\n")
+            detail_file.write("-----------\n\n")
 
         for k in keys_1 & keys_2:
             if d1[k] != d2[k]:
-                print("Different values for key", k, ":")
-                print()
-                print(d1[k])
-                print()
-                print(d2[k])
-                print("-----------\n")
+                detail_file.write(f"Different values for key {k}:\n\n")
+                detail_file.write(f"{d1[k]}\n\n")
+                detail_file.write(f"{d2[k]}\n")
+                detail_file.write("-----------\n\n")
 
     return diff_found
 
-def compare_vars(v1, v2, silent = False, tag = None):
+def compare_vars(v1, v2, silent = False, tag = None, detail_file = sys.stdout):
     """v1 and v2 are numpy arrays. Return True if a difference if
     found."""
     
     if v1.shape == v2.shape:
         if v1.size == 0:
             if not silent:
-                if tag: print(tag, ":")
-                print('Zero-sized array')
-                print("-------------\n")
+                if tag: detail_file.write(f"{tag}:\n")
+                detail_file.write('Zero-sized array\n')
+                detail_file.write("-------------\n\n")
                 
             diff_found = False
         else:
@@ -68,15 +66,15 @@ def compare_vars(v1, v2, silent = False, tag = None):
 
                 diff_found = np.any(compressed1 != compressed2)
 
-            if  diff_found and not silent:
-                if tag: print(tag, ":")
-                print("Different content")
-                print("-------------\n")
+            if diff_found and not silent:
+                if tag: detail_file.write(f"{tag}:\n")
+                detail_file.write("Different content\n")
+                detail_file.write("-------------\n\n")
     else:
         if not silent:
-            if tag: print(tag, ":")
-            print("compare_vars: shapes differ")
-            print("-------------\n")
+            if tag: detail_file.write(f"{tag}:\n")
+            detail_file.write("compare_vars: shapes differ\n")
+            detail_file.write("-------------\n\n")
 
         diff_found = True
 
