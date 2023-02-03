cd8df4c184e8abac4f9aee73e8760220f2f39a31
Lionel GUEZ <guez@lmd.ens.fr>
2023-02-04 00:20:42 +0100

    Bug fix: count differences for tests not re-run

--- test_compare.py
+++ test_compare.py
@@ -254,6 +254,9 @@ def run_tests(my_runs, allowed_keys, compare_dir, other_args):
         
         if path.exists(my_run["title"]) and not previous_failed:
             print("Skipping", my_run["title"], "(already exists, did not fail)")
+            fname = path.join(my_run["title"], "comparison.txt")
+            if path.exists(fname):
+                cumul_return += 1
         else:
             if not set(my_run) <= allowed_keys:
                 print("bad keys:")
