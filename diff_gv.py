c7254c2e206766a9507c6567e7b5b104fb2ac114
Lionel GUEZ <guez@lmd.ens.fr>
2023-03-10 20:59:00 +0100

    Add comparison of edges

--- diff_gv.py
+++ diff_gv.py
@@ -15,6 +15,7 @@ def diff_gv(path_1, path_2, detail_file):
         detail_file.write('\n' + "*" * 10 + '\n\n')
         detail_file.write(f"diff {path_1} {path_2}\n")
         detail_file.write(f"Equality of nodes: {G1.nodes == G2.nodes}\n")
+        detail_file.write(f"Equality of edges: {G1.edges == G2.edges}\n")
         detail_file.write(f"Equality of adjacencies: {G1.adj == G2.adj}\n")
         detail_file.write(f"Equality of attributes: {G1.graph == G2.graph}\n")
 
