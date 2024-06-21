10008a2ad2e2d023011d00b0a03e55d6aa976698
Lionel GUEZ <guez@lmd.ens.fr>
2024-06-21 12:00:47 +0200

    Move function `compare` to separate file
    This is clearer, especially since we are going to also use `compare`
    in a new script.

---
+++ compare_single_test.py
@@ -0,0 +1,45 @@
+import time
+from os import path
+import os
+import sys
+
+import selective_diff
+
+def compare(title, my_run, compare_dir):
+    t0 = time.perf_counter()
+    old_dir = path.join(compare_dir, title)
+    sel_diff_args = {"exclude": []}
+
+    if "sel_diff_args" in my_run:
+        sel_diff_args |= my_run["sel_diff_args"]
+
+    sel_diff_args["exclude"] = sel_diff_args["exclude"][:] + [
+        "timing_test_compare.txt",
+        "comparison.txt",
+    ]
+    # (Copy so  we do not modify my_run["sel_diff_args"]["exclude"].)
+
+    fname = path.join(title, "comparison.txt")
+
+    with open(fname, "w") as f:
+        return_code = selective_diff.selective_diff(
+            [old_dir, title], **sel_diff_args, file_out=f
+        )
+        f.write("\n" + ("*" * 10 + "\n") * 2 + "\n")
+
+    if return_code == 0:
+        os.remove(fname)
+    elif return_code != 1:
+        sys.exit(
+            "Problem in selective_diff.py, return code should be 0 or 1.\n"
+            'See "comparison.txt".'
+        )
+
+    t1 = time.perf_counter()
+    line = "Elapsed time for comparison: {:.0f} s\n".format(t1 - t0)
+    fname = path.join(title, "timing_test_compare.txt")
+
+    with open(fname, "a") as f:
+        f.write(line)
+
+    return return_code
--- test_compare.py
+++ test_compare.py
@@ -102,7 +102,7 @@ import time
 
 import yachalk
 
-import selective_diff
+import compare_single_test
 
 
 def get_all_required(title, my_run):
@@ -323,7 +323,9 @@ def run_tests(my_runs, allowed_keys, compare_dir):
                     try:
                         shutil.copytree(title, old_dir, symlinks=True)
                     except FileExistsError:
-                        return_code = compare(title, my_run, compare_dir)
+                        return_code = compare_single_test.compare(
+                            title, my_run, compare_dir
+                        )
 
                         if return_code != 0:
                             print("difference found")
@@ -344,46 +346,6 @@ def run_tests(my_runs, allowed_keys, compare_dir):
         print("Number not created because of missing requirements:", n_missing)
 
     return cumul_return
-
-
-def compare(title, my_run, compare_dir):
-    t0 = time.perf_counter()
-    old_dir = path.join(compare_dir, title)
-    sel_diff_args = {"exclude": []}
-
-    if "sel_diff_args" in my_run:
-        sel_diff_args |= my_run["sel_diff_args"]
-
-    sel_diff_args["exclude"] = sel_diff_args["exclude"][:] + [
-        "timing_test_compare.txt",
-        "comparison.txt",
-    ]
-    # (Copy so  we do not modify my_run["sel_diff_args"]["exclude"].)
-
-    fname = path.join(title, "comparison.txt")
-
-    with open(fname, "w") as f:
-        return_code = selective_diff.selective_diff(
-            [old_dir, title], **sel_diff_args, file_out=f
-        )
-        f.write("\n" + ("*" * 10 + "\n") * 2 + "\n")
-
-    if return_code == 0:
-        os.remove(fname)
-    elif return_code != 1:
-        sys.exit(
-            "Problem in selective_diff.py, return code should be 0 or 1.\n"
-            'See "comparison.txt".'
-        )
-
-    t1 = time.perf_counter()
-    line = "Elapsed time for comparison: {:.0f} s\n".format(t1 - t0)
-    fname = path.join(title, "timing_test_compare.txt")
-
-    with open(fname, "a") as f:
-        f.write(line)
-
-    return return_code
 
 
 parser = argparse.ArgumentParser(
