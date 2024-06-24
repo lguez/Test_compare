9c385b88b89fa1fe6ebe06aa953971e0c5011dd0
Lionel GUEZ <guez@lmd.ens.fr>
2024-06-24 10:17:55 +0200

    Decide when we consider there is a difference

--- diff_shapes.py
+++ diff_shapes.py
@@ -16,31 +16,37 @@ def diff_shapes(
     tolerance,
 ):
     if s_old.points == s_new.points:
+        diff_found = False
+
         if report_identical:
             detail_file.write(
                 f"\nVertices for shape {i_shape} are identical.\n"
             )
     else:
-        diff_found = True
         detail_file.write(f"\nVertices for shape {i_shape} differ.\n")
 
         if s_old.shapeType == shapefile.NULL:
+            diff_found = True
             detail_file.write("Old shape is NULL.\n")
         elif s_new.shapeType == shapefile.NULL:
+            diff_found = True
             detail_file.write("New shape is NULL.\n")
         else:
             nparts_old = len(s_old.parts)
             nparts_new = len(s_new.parts)
 
             if nparts_old != nparts_new:
+                diff_found = True
                 detail_file.write(
                     f"Numbers of parts in shape {i_shape} differ:"
                     f"{nparts_old} {nparts_new}\n"
                 )
             else:
                 if len(s_old.points) == 0:
+                    diff_found = True
                     detail_file.write(f"No point in old shape {i_shape}\n")
                 elif len(s_new.points) == 0:
+                    diff_found = True
                     detail_file.write(f"No point in new shape {i_shape}\n")
                 else:
                     # Suppress possible warning about orientation
@@ -54,6 +60,8 @@ def diff_shapes(
 
                     if g_old.geom_type == g_new.geom_type:
                         if g_old.geom_type == "MultiPolygon":
+                            ret_code = 0
+
                             for j, (p_old, p_new) in enumerate(
                                 zip(g_old, g_new)
                             ):
@@ -68,6 +76,8 @@ def diff_shapes(
                                     tolerance,
                                     report_identical,
                                 )
+
+                            diff_found = ret_code != 0
                         elif g_old.geom_type == "Polygon":
                             ret_code = compare_poly.compare_poly(
                                 ax,
@@ -79,21 +89,25 @@ def diff_shapes(
                                 tolerance=tolerance,
                                 report_identical=report_identical,
                             )
+                            diff_found = ret_code != 0
                         elif g_old.geom_type == "Point":
                             abs_rel_diff = np.abs(
                                 np.array(g_new.coords) / np.array(g_old.coords)
                                 - 1
                             )
+                            diff_found = abs_rel_diff > tolerance
                             detail_file.write(
                                 "Absolute value of relative difference: "
                                 f"{abs_rel_diff}\n"
                             )
                         else:
+                            diff_found = True
                             detail_file.write(
                                 "Geometry type not supported:"
                                 f"{g_old.geom_type}\n"
                             )
                     else:
+                        diff_found = True
                         detail_file.write(
                             "Geometry types differ:"
                             f"{g_old.geom_type} {g_new.geom_type}\n"
