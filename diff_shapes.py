f9194c876f39ff0176df65c63c5da17412077392
Lionel GUEZ <guez@lmd.ens.fr>
2024-06-24 07:13:27 +0200

    Extract some code to new function `diff_shapes`
    Extract some code from function `diff_shp` to new function
    `diff_shapes`.

---
+++ diff_shapes.py
@@ -0,0 +1,89 @@
+import shapefile
+from shapely import geometry
+import numpy as np
+
+import compare_poly
+
+
+def diff_shapes(
+    s_old, s_new, report_identical, detail_file, i, ax, marker_iter, tolerance
+):
+    if s_old.points == s_new.points:
+        if report_identical:
+            detail_file.write(f"\nVertices for shape {i} are identical.\n")
+    else:
+        diff_found = True
+        detail_file.write(f"\nVertices for shape {i} differ.\n")
+
+        if s_old.shapeType == shapefile.NULL:
+            detail_file.write("Old shape is NULL.\n")
+        elif s_new.shapeType == shapefile.NULL:
+            detail_file.write("New shape is NULL.\n")
+        else:
+            nparts_old = len(s_old.parts)
+            nparts_new = len(s_new.parts)
+
+            if nparts_old != nparts_new:
+                detail_file.write(
+                    f"Numbers of parts in shape {i} differ:"
+                    f"{nparts_old} {nparts_new}\n"
+                )
+            else:
+                if len(s_old.points) == 0:
+                    detail_file.write(f"No point in old shape {i}\n")
+                elif len(s_new.points) == 0:
+                    detail_file.write(f"No point in new shape {i}\n")
+                else:
+                    # Suppress possible warning about orientation
+                    # of polygon (only is effective with version
+                    # >= 2.2.0 of pyshp):
+                    shapefile.VERBOSE = False
+
+                    g_old = geometry.shape(s_old.__geo_interface__)
+                    g_new = geometry.shape(s_new.__geo_interface__)
+                    shapefile.VERBOSE = True
+
+                    if g_old.geom_type == g_new.geom_type:
+                        if g_old.geom_type == "MultiPolygon":
+                            for j, (p_old, p_new) in enumerate(
+                                zip(g_old, g_new)
+                            ):
+                                compare_poly.compare_poly(
+                                    ax,
+                                    p_old,
+                                    p_new,
+                                    i,
+                                    j,
+                                    detail_file,
+                                    marker_iter,
+                                    tolerance,
+                                )
+                        elif g_old.geom_type == "Polygon":
+                            compare_poly.compare_poly(
+                                ax,
+                                g_old,
+                                g_new,
+                                i,
+                                detail_file=detail_file,
+                                marker_iter=marker_iter,
+                                tolerance=tolerance,
+                            )
+                        elif g_old.geom_type == "Point":
+                            abs_rel_diff = np.abs(
+                                np.array(g_new.coords) / np.array(g_old.coords)
+                                - 1
+                            )
+                            detail_file.write(
+                                "Absolute value of relative difference: "
+                                f"{abs_rel_diff}\n"
+                            )
+                        else:
+                            detail_file.write(
+                                "Geometry type not supported:"
+                                f"{g_old.geom_type}\n"
+                            )
+                    else:
+                        detail_file.write(
+                            "Geometry types differ:"
+                            f"{g_old.geom_type} {g_new.geom_type}\n"
+                        )
--- diff_shp.py
+++ diff_shp.py
@@ -6,11 +6,9 @@ import argparse
 import sys
 
 import shapefile
-import numpy as np
 from matplotlib import pyplot as plt
 
-
-
+import diff_shapes
 
 
 def diff_shp(
@@ -48,86 +46,16 @@ def diff_shp(
     for i, (s_old, s_new) in enumerate(
         zip(reader_old.iterShapes(), reader_new.iterShapes())
     ):
-        if s_old.points == s_new.points:
-            if report_identical:
-                detail_file.write(f"\nVertices for shape {i} are identical.\n")
-        else:
-            diff_found = True
-            detail_file.write(f"\nVertices for shape {i} differ.\n")
-
-            if s_old.shapeType == shapefile.NULL:
-                detail_file.write("Old shape is NULL.\n")
-            elif s_new.shapeType == shapefile.NULL:
-                detail_file.write("New shape is NULL.\n")
-            else:
-                nparts_old = len(s_old.parts)
-                nparts_new = len(s_new.parts)
-
-                if nparts_old != nparts_new:
-                    detail_file.write(
-                        f"Numbers of parts in shape {i} differ:"
-                        f"{nparts_old} {nparts_new}\n"
-                    )
-                else:
-                    if len(s_old.points) == 0:
-                        detail_file.write(f"No point in old shape {i}\n")
-                    elif len(s_new.points) == 0:
-                        detail_file.write(f"No point in new shape {i}\n")
-                    else:
-                        # Suppress possible warning about orientation
-                        # of polygon (only is effective with version
-                        # >= 2.2.0 of pyshp):
-                        shapefile.VERBOSE = False
-
-                        g_old = geometry.shape(s_old.__geo_interface__)
-                        g_new = geometry.shape(s_new.__geo_interface__)
-                        shapefile.VERBOSE = True
-
-                        if g_old.geom_type == g_new.geom_type:
-                            if g_old.geom_type == "MultiPolygon":
-                                for j, (p_old, p_new) in enumerate(
-                                    zip(g_old, g_new)
-                                ):
-                                    compare_poly(
-                                        ax,
-                                        p_old,
-                                        p_new,
-                                        i,
-                                        j,
-                                        detail_file,
-                                        marker_iter,
-                                        tolerance,
-                                    )
-                            elif g_old.geom_type == "Polygon":
-                                compare_poly(
-                                    ax,
-                                    g_old,
-                                    g_new,
-                                    i,
-                                    detail_file=detail_file,
-                                    marker_iter=marker_iter,
-                                    tolerance=tolerance,
-                                )
-                            elif g_old.geom_type == "Point":
-                                abs_rel_diff = np.abs(
-                                    np.array(g_new.coords)
-                                    / np.array(g_old.coords)
-                                    - 1
-                                )
-                                detail_file.write(
-                                    "Absolute value of relative difference: "
-                                    f"{abs_rel_diff}\n"
-                                )
-                            else:
-                                detail_file.write(
-                                    "Geometry type not supported:"
-                                    f"{g_old.geom_type}\n"
-                                )
-                        else:
-                            detail_file.write(
-                                "Geometry types differ:"
-                                f"{g_old.geom_type} {g_new.geom_type}\n"
-                            )
+        diff_shapes.diff_shapes(
+            s_old,
+            s_new,
+            report_identical,
+            detail_file,
+            i,
+            ax,
+            marker_iter,
+            tolerance,
+        )
 
     detail_file.write("\n")
 
